b4b1bd05a95698ad49ee68332305cf5acf708080
Xynnn007 <xynnn@linux.alibaba.com>
2023-10-10 15:31:29 +0800

    refactor: some update
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- scripts/tdx/kernel.py
+++ scripts/tdx/kernel.py
@@ -2,27 +2,39 @@
 # coding=utf-8
 # Copyright (c) 2023, Alibaba Inc.
 
-# Usage: python simple_ref.py <path-to-raw-mrtd-file> <name-of-reference-value> <output-file>
+# Usage: python kernel.py <path-to-kernel> <length-of-payload-defined-intd-shim> <output-file>
 
-import json
 import sys
+import hashlib
 
 sys.path.append('../')
 
 import lib
 
 SAMPLE_REFERENCE_HASH = "tdx.ccel.kernel"
+PAYLOAD_LENGTH = eval(sys.argv[2])
+
+def kernel_hash(kernel_file):
+    hashbuf = hashlib.sha384()
+    with open(kernel_file, 'rb') as kernel:
+        content = kernel.read()
+        padding_length = PAYLOAD_LENGTH - len(content)
+        if padding_length < 0:
+            exit(-1)
+        hashbuf.update(content)
+        hashbuf.update(b'\0' * padding_length)
+    return hashbuf.hexdigest()
 
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print("illegal input args")
         exit(-1)
 
-    kernel_hash = sys.argv[1]
-    output = sys.argv[2]
+    kernel_file = sys.argv[1]
+    output = sys.argv[3]
 
     struct = {
-        SAMPLE_REFERENCE_HASH: [kernel_hash]
+        SAMPLE_REFERENCE_HASH: [kernel_hash(kernel_file)]
     }
     provenance = lib.generate_sample_provenance(struct)
     with open(output, 'w+') as outfile:
